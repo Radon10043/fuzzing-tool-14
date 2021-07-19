469d4a1fc961453c91d19f2859304df7bbd90cef
yagol2020 <yhw_yagol@yeah.net>
2021-07-20 02:32:11 +0800

    add struct comment: support multi comment

--- util/get_comment_from_struct.py
+++ util/get_comment_from_struct.py
@@ -1,12 +1,11 @@
 import re
 
 singe_comment_patten = '//.*'  # 标准匹配单行注释
-# multi_comment_patten = '\/\*(?:[^\*]|\*+[^\/\*])*\*+\/'  # 标准匹配多行注释  可匹配跨行注释
+multi_comment_patten = '\/\*(?:[^\*]|\*+[^\/\*])*\*+\/'  # 标准匹配多行注释  可匹配跨行注释
 
 singe_comment_re = re.compile(singe_comment_patten)  # 单行注释
 
-
-# multi_comment_re = re.compile(multi_comment_patten)  # 编译正则表达式
+multi_comment_re = re.compile(multi_comment_patten)  # 编译正则表达式
 
 
 def get_code_str_list(header_path_list):
@@ -51,7 +50,11 @@ def handle_struct(struct_dict: dict):
                 code_file_str = open(code_file_path, encoding="gbk", mode="r").readlines()[int(code_line_number) - 1]
             match_result = singe_comment_re.findall(code_file_str)
             if len(match_result) == 0:
+                match_result = multi_comment_re.findall(code_file_str)
+            if len(match_result) == 0:
                 struct_dict[struct_name][var_type_name]["comment"] = "该成员变量没有注释说明"
             else:
-                struct_dict[struct_name][var_type_name]["comment"] = match_result[0].replace("//", "")
+                struct_dict[struct_name][var_type_name]["comment"] = match_result[0].replace("//", "").replace("/*",
+                                                                                                               "").replace(
+                    "*/", "").strip()
     return struct_dict
