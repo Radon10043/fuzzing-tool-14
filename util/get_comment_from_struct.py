f64bdde7c562d9cca3e899c00cf489784a806a2c
yagol2020 <yhw_yagol@yeah.net>
2021-07-24 02:43:19 +0800

    todo: no name var

--- util/get_comment_from_struct.py
+++ util/get_comment_from_struct.py
@@ -15,6 +15,8 @@ multi_comment_patten = '\/\*(?:[^\*]|\*+[^\/\*])*\*+\/'  # 标准匹配多行注
 singe_comment_re = re.compile(singe_comment_patten)  # 单行注释
 
 multi_comment_re = re.compile(multi_comment_patten)  # 编译正则表达式
+remove_keywords = ["char", "double", "enum", "float", "int", "long", "short", "unsigned", "struct", "union", "signed",
+                   "void", ";"]
 
 
 def handle_struct(struct_dict: dict):
@@ -34,18 +36,20 @@ def handle_struct(struct_dict: dict):
     -----
     [description]
     """
+    unknown_var_info = list()
+    struct_name = None
     for struct_name in struct_dict.keys():
+        struct_name = struct_name
         for var_type_name in struct_dict[struct_name].keys():
             var_path_name = var_type_name.split(" ")[-1]
             var_name = var_path_name.split(".")[-1].split(":")[0]  # 该成员变变量的名字
             try:
-                if struct_dict[struct_name][var_type_name]["loc"] is None:
-                    raise sa.VariableNoNameError
-                code_file_path = struct_dict[struct_name][var_type_name]["loc"].split("?")[0]
-                code_line_number = struct_dict[struct_name][var_type_name]["loc"].split("?")[1]
-            except sa.VariableNoNameError:
-                struct_dict[struct_name][var_type_name]["comment"] = "该成员变量没有名字"
-                continue
+                if "*" in struct_dict[struct_name][var_type_name]["loc"]:
+                    unknown_var_info.append(var_type_name)
+                    continue
+                else:
+                    code_file_path = struct_dict[struct_name][var_type_name]["loc"].split("?")[0]
+                    code_line_number = struct_dict[struct_name][var_type_name]["loc"].split("?")[1]
             except BaseException as e:
                 print("\033[1;31m")
                 traceback.print_exc()
@@ -69,5 +73,46 @@ def handle_struct(struct_dict: dict):
             else:
                 struct_dict[struct_name][var_type_name]["comment"] = match_result[0].replace("//", "").replace("/*",
                                                                                                                "").replace(
-                    "*/", "").strip()
+                    "*/", "").strip("*")
+    for var_type_name in unknown_var_info:
+        var_info = struct_dict[struct_name][var_type_name]
+        file_path = var_info["loc"].split("*")[0]
+        struct_end_line_number = var_info["loc"].split("*")[1]
+        try:
+            file_content = open(file_path, mode="r", encoding="gbk").readlines()
+        except:
+            file_content = open(file_path, mode="r", encoding="utf").readlines()
+        # 定位这个结构体的范围
+        line_number = int(struct_end_line_number) - 2
+        while True:
+            handle_line = file_content[line_number].strip()
+            if len(handle_line) == 0:
+                line_number -= 1
+                continue
+            handle_line_split = handle_line.split()
+            if "typedef" in handle_line_split or "struct" in handle_line_split:
+                break
+            elif ":" in handle_line:
+                handle_line_backup = handle_line
+                # 确认是否是无名变量
+                # TODO 找到了无名变量和他的备注，但是只反着找到了第一个，需要为无名变量编号
+                for remove_keyword in remove_keywords:
+                    handle_line = handle_line.replace(remove_keyword, "").strip()
+                handle_line.replace(" ", "").strip()
+                handle_line_split_contain_empty = handle_line.split(":")
+                handle_line_split = list()
+                for one in handle_line_split_contain_empty:
+                    if len(one) != 0:
+                        handle_line_split.append(one)
+                if len(handle_line_split) == 1:
+                    match_result = singe_comment_re.findall(handle_line_backup)
+                    if len(match_result) == 0:
+                        match_result = multi_comment_re.findall(code_file_str)
+                    if len(match_result) == 0:
+                        comment_result = "无名变量没有注释说明"
+                    else:
+                        comment_result = match_result[0].replace("//", "").replace("/*", "").replace("*/", "").strip(
+                            "*")
+                    struct_dict[struct_name][var_type_name]["comment"] = comment_result
+            line_number -= 1
     return struct_dict
