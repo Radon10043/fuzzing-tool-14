21a19ebbcad04eb8cd39e9b3547d17396198deb6
yagol2020 <yhw_yagol@yeah.net>
2021-08-11 23:15:52 +0800

    temp file: test py socket, get udp report from c++(LAN)

--- util/get_udp_from_cpp.py
+++ util/get_udp_from_cpp.py
@@ -9,12 +9,15 @@ i = 0
 while (i < 100000):
     i += 1
     # 3. 接收数据
-    recv_data = udp_socket.recvfrom(1024)
-    recv_msg = recv_data[0]
-    send_addr = recv_data[1]
+    try:
+        recv_data = udp_socket.recvfrom(1024)
+        recv_msg = recv_data[0]
+        send_addr = recv_data[1]
 
-    # 4. 打印接收到的信息
-    print("%s:%s" % (str(send_addr), str(recv_msg.decode("utf-8"))))
+        # 4. 打印接收到的信息
+        print("%s:%s" % (str(send_addr), str(recv_msg)))
+    except Exception as e:
+        print(e)
 
 # 5. 关闭套接字
 udp_socket.close()
